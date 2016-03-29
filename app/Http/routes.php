d45fe65218c8980298e15d218cb0bd5d4dccf30c
unknown <Administrator@PC08.BeijingSecGroup.local>
2016-03-29 17:51:57 +0800

    add .env to gitignore and change route setting

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,13 +23,10 @@
 use App\Classes\Test;
 
 Route::get('/call_me', function(Test $test) {
-	echo $test->call_me();
+    echo $test->call_me();
 });
 
-
-
 /*Infocast API for Index*/
-
 /*API CALL*/
 Route::get('/schedule/requestmostactive', ['uses' => 'InfocastController@requestMostActive', 'as' => 'schedule.requestmostactive']);
 Route::get('/schedule/requestmarketnews', ['uses' => 'InfocastController@requestMarketNews', 'as' => 'schedule.requestmarketnews']);
@@ -44,37 +41,27 @@ Route::get('/mail/test_mail_out', 'EmailController@test_mail_out')->name('email.
 Route::get('/mail/test_mail_in', 'EmailController@test_mail_in')->name('email.test_mail_in');
 
 
+Route::group(['middleware' => ['web']], function () {
 
+    /*Static Page inside controller*/
+    Route::get('/', ['uses' => 'PageController@homepage', 'as' => 'front.homepage']);
+    Route::get('/market-news', ['uses' => 'PageController@marketnews', 'as' => 'front.marketnews']);
+    Route::get('/company-structure', ['uses' => 'PageController@companystructure', 'as' => 'front.companystructure']);
+    Route::get('/download', ['uses' => 'PageController@download', 'as' => 'front.download']);
+    Route::get('/about-us', ['uses' => 'PageController@aboutus', 'as' => 'front.aboutus']);
+    Route::get('/contact-us', ['uses' => 'PageController@contactus', 'as' => 'front.contactus']);
 
-/*Static Page inside controller*/
-Route::get('/', ['uses' => 'PageController@homepage', 'as' => 'front.homepage']);
-Route::get('/market-news', ['uses' => 'PageController@marketnews', 'as' => 'front.marketnews']);
-Route::get('/company-structure', ['uses' => 'PageController@companystructure', 'as' => 'front.companystructure']);
-Route::get('/download', ['uses' => 'PageController@download', 'as' => 'front.download']);
-Route::get('/about-us', ['uses' => 'PageController@aboutus', 'as' => 'front.aboutus']);
-Route::get('/contact-us', ['uses' => 'PageController@contactus', 'as' => 'front.contactus']);
+    /*Route for sending enquiry to email*/
+    Route::post('/mail/send_enquiry', 'EmailController@send_web_enquiry')->name('email.send_web_enquiry');
 
-/*Route for sending enquiry to email*/
-Route::post('/mail/send_enquiry', 'EmailController@send_web_enquiry')->name('email.send_web_enquiry');
+    /*Admin Page*/
+    Route::get('/server/monitor', 'ServerMonitorController@index')->name('server-monitor');
+    Route::get('/server/checkserverstatus', 'ServerMonitorController@checkServerStatus')->name('server-status');
 
-
-
-
-
-
-Route::group(['domain' => 'admin.beijingsecgroup.com', 'middleware' => ['web']], function () {
-
-  Route::get('/', array('as' => 'admin-homepage', function () {
-	  echo 'This is admin page';
-	}));
-
-	Route::get('/server/monitor', 'ServerMonitorController@index')->name('server-monitor');
-	Route::get('/server/checkserverstatus', 'ServerMonitorController@checkServerStatus')->name('server-status');
-
-	Route::get('/test', array('as' => 'admin-homepage', function () {
-	  return view('backend.test');
-	}));
-	Route::post('/server/checkconnectionbyip', 'ServerMonitorController@checkConnectionByIP');
+    Route::get('/test', array('as' => 'admin-homepage', function () {
+        return view('backend.test');
+    }));
+    Route::post('/server/checkconnectionbyip', 'ServerMonitorController@checkConnectionByIP');
 
 });
 
