ed8b3c6b3fed15ee65fdbf71b6e2d42e2a0087a8
unknown <Administrator@PC08.BeijingSecGroup.local>
2016-04-20 10:25:20 +0800

    Maintenance Page

---
+++ public/bsg_maintenance/index.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+	<link rel="icon" href="favicon.ico">
+</head>
+<body>
+<style>
+	body {
+		/*background-color: #2c5aa4;*/
+	}
+
+	div {
+		text-align: center;
+		/*color: #FFFFFF;*/
+	}
+
+	.div0 img {
+		height: 200px;
+	}
+
+	.div0 {
+		margin: 140px 0px 10px;
+	}
+
+	.div1 {
+    margin: 42px 0px 34px;
+		font-size: 50px;
+		color: #093682;
+		font-weight: bold;
+	}
+
+	.div2 {
+		font-size: 35px;
+		color: #093682;
+	}
+</style>
+
+<div class="div0"><img src="mobile_bsg_logo_512x512.png"></div>
+
+<div class="div1">欢迎来到北京证券集团有限公司网页</div>
+
+<div class="div2"><img src="under-construction-icon.jpg" style="height: 100px;"> <span style="position: relative; top: -40px;">对不起，本网站正在维护中</span></div>
+</body>
+</html>
