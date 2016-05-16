506b1ae6d194418cd165b386bcea5e006f9708fc
Roy Ho <kwanyin2000@gmail.com>
2016-05-16 15:49:51 +0800

    change mobile nav menu

--- resources/views/frontend/template/header.blade.php
+++ resources/views/frontend/template/header.blade.php
@@ -121,7 +121,7 @@
 
       <!-- Navigation Bar -->
       <nav class="navbar navbar-default main_navbar ">
-        <div class="container-fluid">
+        <div class="container-fluid navbar-main">
           <!-- Brand and toggle get grouped for better mobile display -->
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -144,41 +144,23 @@
               <li><a href="{{route('front.companystructure')}}">{{ trans('web.company_structure') }}</a></li>
               <li><a href="{{route('front.download')}}">{{ trans('web.form_download') }}</a></li>
               <li><a href="{{route('front.contactus')}}">{{ trans('web.contact_us') }}</a></li>
-              <!-- <li><a href="/beijingsecgorup/cs">客户服务</a></li>  -->
-
-              <!-- <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li> -->
             </ul>
-            <!-- <form class="navbar-form navbar-left" role="search">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search">
-              </div>
-              <button type="submit" class="btn btn-default">Submit</button>
-            </form> -->
             <ul class="nav navbar-nav navbar-right">
               <li><a href="#">{{ trans('web.customer_hot_line') }} (852) 3899 1373</a></li>
-
-             <!--  <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                </ul>
-              </li> -->
             </ul>
           </div><!-- /.navbar-collapse -->
+
+
         </div><!-- /.container-fluid -->
+
+        <div class="col-md-12 nav-custom" style="display: none;">
+          <ul class="nav navbar-nav">
+            <!-- <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li> -->
+            <li><a href="{{route('front.homepage')}}">{{ trans('web.home') }}</a></li>
+            <li><a href="{{route('front.aboutus')}}">{{ trans('web.about_us') }}</a></li>
+            <li><a href="{{route('front.marketnews')}}">{{ trans('web.market_news') }}</a></li>
+            <li><a href="{{route('front.companystructure')}}">{{ trans('web.company_structure') }}</a></li>
+            <li><a href="{{route('front.download')}}">{{ trans('web.form_download') }}</a></li>
+            <li><a href="{{route('front.contactus')}}">{{ trans('web.contact_us') }}</a></li>
+          </ul></div>
       </nav>
