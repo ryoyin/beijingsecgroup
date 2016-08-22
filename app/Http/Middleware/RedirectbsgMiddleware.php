79eca5802956c1da141c5c791f5a2cb83ef6ae20
Roy Ho <kwanyin2000@gmail.com>
2016-08-22 14:49:12 +0800

    test redirect non-exist sub 1

--- app/Http/Middleware/RedirectbsgMiddleware.php
+++ app/Http/Middleware/RedirectbsgMiddleware.php
@@ -16,13 +16,18 @@ class RedirectbsgMiddleware
      */
     public function handle($request, Closure $next)
     {
+        $exp_name = explode(".", $_SERVER['SERVER_NAME']);
+        if($exp_name[0] != "www" && $exp_name[1] != "bjsthkgroup") {
+            return redirect('http://www.bjsthkgroup.com');
+        }
+
         //check server addr not dev and contain old domain name beijingsecgroup
         $ips = array('127.0.0.1', '192.168.10.30');
         if(!in_array($_SERVER['SERVER_ADDR'], $ips) && strpos($_SERVER['SERVER_NAME'], 'bjsthkgroup.com') === FALSE) {
             throw new HttpException(301);
         }
 
-//        echo $_SERVER['SERVER_NAME'];
+        //        echo $_SERVER['SERVER_NAME'];
 
         //Power Outage Message
         /*if(!session()->get('power_outage_notice')) {
